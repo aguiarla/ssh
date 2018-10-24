04e234a15c345a9993b5c95e72997f4158df39f6
aguiarla <leandroaguiar@biologo.bio.br>
2018-10-24 14:05:52 -0300

    Update ssh_server.py

--- ssh_server.py
+++ ssh_server.py
@@ -12,3 +12,18 @@ server@server$ jupyter notebook --no-browser --port=8889
 
 client@client$ ssh -N -f -L localhost:8000:localhost:8889 user@IPSERVER
 client@client$ firefox http://localhost:8000
+
+#ou pode ser da seguinte forma:
+
+#Apos criar o arquivo de configuração e definir a senha:
+
+cd. jupyter #entra na pasta que possui o arquivo de configuração
+
+nano jupyter_notebook_config.py
+
+#adicionar essas linhas
+
+c.NotebookApp.port = 10111
+c.NotebookApp.allow_origin = '*'
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.open_browser = False
